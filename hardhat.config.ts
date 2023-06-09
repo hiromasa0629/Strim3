922ecdc28719647dfc618cd02405171158bdb050
hiromasa <howzheng0629@gmail.com>
2023-06-09 11:04:15 +0800

    feat: env & config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from 'dotenv';
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+	paths: {
+    artifacts: "./app/artifacts",
+  },
+	defaultNetwork: "localhost",
+	networks: {
+		sepolia: {
+			url: process.env.ALCHEMY_SEPOLIA_URL,
+			accounts: [process.env.SEPOLIA_PRIVATE_KEY!]
+		}
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY
+	}
 };
 
 export default config;
